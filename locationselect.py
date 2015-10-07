0c1efa6cbab24d3ba431dfd72355125c4d69a9db
Micheál <micheal.boland@ucdconnect.ie>
2015-10-07 21:54:11 +0100

    Successfully created function to calculate distance between two points from lats and lons

--- locationselect.py
+++ locationselect.py
@@ -14,13 +14,42 @@ Date: 3/10/2015
 
 """
 
-# Importing sqlite3 module
+#----------------------------------------------------------------------------------------
+# Function to calculate distance between two point from their latitudes and longitudes
+# This function uses the Haversines formula
+# Inputs: lat1, lon1: latitudes and longitudes of location 1
+#         lat2, lon2: latitude and longitude of location 2
+# Output: dist_calc: distance between the two points
+
+def dist_calc(lat1,lon1,lat2,lon2):
+    # Splitting Havesine formula into two parts for conciseness.    
+    h = ((math.sin((math.radians(lat2)-math.radians(lat1))/2))**2) + \
+    math.cos(math.radians(lat1))*math.cos(math.radians(lat2))* \
+    ((math.sin((math.radians(lon2)-math.radians(lon1))/2))**2)   
+        
+    # h multiplied by radius of Earth which is 6371 km
+    return 2*6371*math.asin(math.sqrt(h))
+    
+#----------------------------------------------------------------------------------------
+
+# Importing relevant modules
 import sqlite3
+import numpy as np
+import math  
 
 # Read in values of location from location table on renewable.db
 conn = sqlite3.connect("renewable.db")
 curs = conn.cursor()
-lat = curs.execute("select lat from location")
+loc_lat = curs.execute("select * from location")
 
-print lat
+for i in loc_lat:
+    print i
+
+# Distance between these points ~1.05 km
+lt1 = 53.355
+ln1 = -6.448
+lt2 = 53.356
+ln2 = -6.433
+
+print dist_calc(lt1,ln1,lt2,ln2)
     
